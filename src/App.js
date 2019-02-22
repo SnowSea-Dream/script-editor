7d6bfc0307cb1f6cb32607704faa128ad51f3cfa
SnowSea <snowsea.dream@gmail.com>
2019-02-22 04:17:25 +0400

    get entities after editing

--- src/App.js
+++ src/App.js
@@ -3,11 +3,145 @@ import Editor from './Editor'
 
 import './App.scss'
 
+const data = {
+  words: [
+    {
+      word: 'My',
+      startTime: 0.7,
+      endTime: 1.1
+    },
+    {
+      word: ' ',
+      startTime: null,
+      endTime: null
+    },
+    {
+      word: 'name',
+      startTime: 1.1,
+      endTime: 1.4
+    },
+    {
+      word: ' ',
+      startTime: null,
+      endTime: null
+    },
+    {
+      word: 'is',
+      startTime: 1.4,
+      endTime: 1.8
+    },
+    {
+      word: ' ',
+      startTime: null,
+      endTime: null
+    },
+    {
+      word: 'Joe',
+      startTime: 1.8,
+      endTime: 2
+    },
+    {
+      word: ' ',
+      startTime: null,
+      endTime: null
+    },
+    {
+      word: 'Smith',
+      startTime: 2,
+      endTime: 2.3
+    },
+    {
+      word: ' ',
+      startTime: null,
+      endTime: null
+    },
+    {
+      word: 'and',
+      startTime: 2.3,
+      endTime: 2.7
+    },
+    {
+      word: ' ',
+      startTime: null,
+      endTime: null
+    },
+    {
+      word: 'now',
+      startTime: 2.7,
+      endTime: 2.8
+    },
+    {
+      word: ' ',
+      startTime: null,
+      endTime: null
+    },
+    {
+      word: 'is',
+      startTime: 2.8,
+      endTime: 3.1
+    },
+    {
+      word: ' ',
+      startTime: null,
+      endTime: null
+    },
+    {
+      word: 'the',
+      startTime: 3.1,
+      endTime: 3.2
+    },
+    {
+      word: ' ',
+      startTime: null,
+      endTime: null
+    },
+    {
+      word: 'the',
+      startTime: 3.2,
+      endTime: 3.4
+    },
+    {
+      word: ' ',
+      startTime: null,
+      endTime: null
+    },
+    {
+      word: 'time',
+      startTime: 3.4,
+      endTime: 4
+    },
+    {
+      word: ' ',
+      startTime: null,
+      endTime: null
+    },
+    {
+      word: 'to',
+      startTime: 4,
+      endTime: 4.2
+    },
+    {
+      word: ' ',
+      startTime: null,
+      endTime: null
+    },
+    {
+      word: 'consider.',
+      startTime: 4.2,
+      endTime: 4.6
+    },
+    {
+      word: ' ',
+      startTime: null,
+      endTime: null
+    }
+  ]
+}
 class App extends Component {
   render () {
     return (
       <div className='p-3'>
-        <Editor />
+        <Editor words={data.words} />
       </div>
     )
   }
--- src/Editor/index.js
+++ src/Editor/index.js
@@ -1,19 +1,90 @@
 import React from 'react'
-import { Editor, EditorState } from 'draft-js'
+import { Editor, EditorState, Modifier, SelectionState } from 'draft-js'
 
 import './styles.scss'
 
 export default class MyEditor extends React.Component {
   constructor (props) {
     super(props)
-    this.state = { editorState: EditorState.createEmpty() }
-    this.onChange = editorState => this.setState({ editorState })
+
+    this.state = {
+      editorState: EditorState.createEmpty()
+    }
   }
+
+  componentDidMount () {
+    this.words2EditorState(this.props.words)
+  }
+
+  words2EditorState (words) {
+    let editorState = EditorState.createEmpty()
+    let contentState = editorState.getCurrentContent()
+    words.forEach(word => {
+      contentState = contentState.createEntity('WORD', 'MUTABLE', word)
+      const entityKey = contentState.getLastCreatedEntityKey()
+      const selectionState = editorState.getSelection()
+      contentState = Modifier.insertText(
+        contentState,
+        selectionState,
+        word.word,
+        null,
+        entityKey
+      )
+
+      editorState = EditorState.push(
+        editorState,
+        contentState,
+        'insert-fragment'
+      )
+    })
+
+    this.setState({ editorState })
+  }
+
+  editorState2Words = () => {
+    const { editorState } = this.state
+    const contentState = editorState.getCurrentContent()
+    let entities = []
+
+    contentState.getBlockMap().forEach(block => {
+      block.findEntityRanges(
+        character => {
+          return true
+        },
+        (start, end) => {
+          const entityKey = block.getEntityAt(start)
+          entities.push({
+            start: start,
+            end: end,
+            editedString: block.getText().slice(start, end),
+            data: entityKey
+              ? contentState.getEntity(entityKey).getData()
+              : undefined
+          })
+        }
+      )
+    })
+
+    // TODO: Need to refine.
+    console.log(entities)
+  }
+
+  onChange = editorState => {
+    const contentState = editorState.getCurrentContent()
+    this.setState({ editorState })
+  }
+
   render () {
+    const { editorState } = this.state
     return (
-      <div className='se-editor'>
-        <Editor editorState={this.state.editorState} onChange={this.onChange} />
-      </div>
+      <>
+        <button className='mb-2' onClick={this.editorState2Words}>
+          Export
+        </button>
+        <div className='se-editor'>
+          <Editor editorState={editorState} onChange={this.onChange} />
+        </div>
+      </>
     )
   }
 }
